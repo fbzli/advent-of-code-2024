8ef8627f7a9228786c548b3afd27cb7505e3ed68
Florian Buenzli <buenzli@ubique.ch>
2024-12-04 17:33:02 +0100

    day 04 unicode fun

--- src/Day04.kt
+++ src/Day04.kt
@@ -13,16 +13,17 @@ fun main() = day(4) {
 		}
 	}
 
+	@Suppress("NonAsciiCharacters", "LocalVariableName")
 	part2 {
 		val input = readLines().unzipChars()
 		input.countIndexed { (x, y), c ->
-			c == 'A' && (
-					(input.getOrNull(x - 1, y - 1) == 'M' && input.getOrNull(x + 1, y + 1) == 'S' ||
-							input.getOrNull(x - 1, y - 1) == 'S' && input.getOrNull(x + 1, y + 1) == 'M')
-							&&
-							(input.getOrNull(x - 1, y + 1) == 'M' && input.getOrNull(x + 1, y - 1) == 'S' ||
-									input.getOrNull(x - 1, y + 1) == 'S' && input.getOrNull(x + 1, y - 1) == 'M')
-					)
+			c == 'A' && run {
+				val `◣` = input.getOrNull(x - 1, y + 1)
+				val `◥` = input.getOrNull(x + 1, y - 1)
+				val `◢` = input.getOrNull(x + 1, y + 1)
+				val `◤` = input.getOrNull(x - 1, y - 1)
+				(`◤` == 'M' && `◢` == 'S' || `◤` == 'S' && `◢` == 'M') && (`◣` == 'M' && `◥` == 'S' || `◣` == 'S' && `◥` == 'M')
+			}
 		}
 	}
 
