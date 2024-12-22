204563523d2368fe8b9424d6dc5a16fea41b2605
Florian Buenzli <buenzli@ubique.ch>
2024-12-22 14:40:59 +0100

    day 22 but in fast

--- src/Day22.kt
+++ src/Day22.kt
@@ -1,6 +1,4 @@
 import utils.day
-import utils.indexOfSubList
-import utils.printProgress
 
 fun main() = day(22) {
 
@@ -17,43 +15,33 @@ fun main() = day(22) {
 	}
 
 	part2 {
-		val prices = readLines().map { seed ->
+		val monkeys = readLines().map { seed ->
 			var secret = seed.toLong()
-			buildList {
+			val deltas = mutableListOf<Long>()
+			val prices = buildList {
 				add(secret % 10)
 				repeat(2000) {
 					secret = ((secret * 64) xor secret) % 16777216
 					secret = ((secret / 32) xor secret) % 16777216
 					secret = ((secret * 2048) xor secret) % 16777216
 					add(secret % 10)
+					deltas.add(get(size - 1) - get(size - 2))
 				}
 			}
+			prices to deltas
 		}
-		val monkeys = prices.map { it to it.windowed(2).map { (a, b) -> (b - a).toInt() } }
-		val sequences = buildList {
-			val deltaRangeGenerated = -3..3 // assuming the optimal delta sequence only contains -3 to 3
-			val deltaRangeValid = -9..9
-			for (a in deltaRangeGenerated) {
-				for (b in deltaRangeGenerated) {
-					for (c in deltaRangeGenerated) {
-						for (d in deltaRangeGenerated) {
-							if ((a + b + c + d) !in deltaRangeValid) continue
-							add(listOf(a, b, c, d))
-						}
+		val sequenceSum = mutableMapOf<List<Long>, Long>()
+		monkeys.forEach { (prices, deltas) ->
+			deltas.windowed(4)
+				.mapIndexed { i, sequence -> i to sequence }
+				.distinctBy { it.second }
+				.forEach { (i, sequence) ->
+					sequenceSum.compute(sequence) { sequence, oldValue ->
+						(oldValue ?: 0) + prices[i + 4]
 					}
 				}
-			}
 		}
-		var i = 0
-		printProgress(i++, sequences.size)
-		sequences.parallelStream().map { sequence ->
-			printProgress(i++, sequences.size)
-			monkeys.sumOf { (prices, deltas) ->
-				val deltaIndex = deltas.indexOfSubList(sequence).takeIf { it >= 0 } ?: return@sumOf 0
-				val priceIndex = deltaIndex + 4
-				prices[priceIndex].toInt()
-			}
-		}.max(Int::compareTo).get()
+		sequenceSum.maxOf { it.value }
 	}
 
 }
